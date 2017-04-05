a3875616d64d1df2596bb8f47be3b017f28f730a
Zyan <coderZyan@outlook.com>
2017-04-05 10:57:38 +0800

    增加弹出框

--- 获取app端图像数据/EE/app/server.js
+++ 获取app端图像数据/EE/app/server.js
@@ -1,5 +1,7 @@
 // todo:https设置。
-const http = require('http');
+const https = require('https'),
+      fs = require('fs'),
+      path = require('path');
 
 const port = 8081;
 
@@ -7,11 +9,15 @@ const data = {
     name:'Zyan'
 }
 
+var privateKey  = fs.readFileSync(path.join(__dirname, '../certificate/private.pem'), 'utf8');  
+var certificate = fs.readFileSync(path.join(__dirname, '../certificate/file.crt'), 'utf8');  
+var credentials = {key: privateKey, cert: certificate};  
+
 const resHander = (req,res) => {
     res.writeHead(200,{'Content-Type':'application/json'});
     res.end(JSON.stringify(data));
 }
-const server = http.createServer(resHander);
+const server = https.createServer(credentials,resHander);
 
 server.listen(port,() => {
     console.log('服务器已经开启');
