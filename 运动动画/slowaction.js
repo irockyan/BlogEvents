bdb7a9dfc25a9f5af57fedc69ec7287f778b16fb
Zyan <coderZyan@outlook.com>
2017-06-25 09:43:55 +0800

    鼠标跟随

--- 运动动画/slowaction.js
+++ 运动动画/slowaction.js
@@ -1,11 +1,17 @@
 function slowAction(ele, target) {
-    let dir = target.direction ? 'left' : 'top'
-    let direction =  target.direction ? 'offsetLeft' : 'offsetTop'
+    let dir = target.direction
+        ? 'left'
+        : 'top'
+    let direction = target.direction
+        ? 'offsetLeft'
+        : 'offsetTop'
     clearInterval(ele.timer)
     ele.timer = setInterval(function () {
         var errorLength = target.length - ele[direction]
-        var step = errorLength/10
-        step = step > 0 ? Math.ceil(step) : Math.floor(step)
+        var step = errorLength / 10
+        step = step > 0
+            ? Math.ceil(step)
+            : Math.floor(step)
         ele.style[dir] = ele[direction] + step + 'px'
         if (Math.abs(errorLength) < Math.abs(step)) {
             ele.style[dir] = target.length + 'px'
