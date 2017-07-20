36651cda2e7a3ca8a9c69b54e167519bb8d786d5
Zyan <coderZyan@outlook.com>
2017-07-20 17:39:29 +0800

    更新主页

--- Readme.md
+++ Readme.md
@@ -25,11 +25,6 @@
 1. [三栏式布局](./常见页面结构/三栏式布局/)
 1. [CSS安卓机器人](./CSS实现安卓机器人/index.html)
 
-## 百度IFE练习
-
-1. [零基础HTML编码](./HTMLElements/零基础HTML编码/01.html)
-1. [通过HTML和CSS模拟报纸排版](./HTMLElements/通过HTML和CSS模拟报纸排版/index.html)
-
 ## 博客专用
 
 1. [项目架构](./项目架构)
--- 运动动画/slowaction.js
+++ 运动动画/slowaction.js
@@ -1,21 +1,19 @@
-function slowAction(ele, target) {
-    let dir = target.direction
-        ? 'left'
-        : 'top'
-    let direction = target.direction
-        ? 'offsetLeft'
-        : 'offsetTop'
-    clearInterval(ele.timer)
-    ele.timer = setInterval(function () {
-        var errorLength = target.length - ele[direction]
-        var step = errorLength / 10
-        step = step > 0
-            ? Math.ceil(step)
-            : Math.floor(step)
-        ele.style[dir] = ele[direction] + step + 'px'
-        if (Math.abs(errorLength) < Math.abs(step)) {
-            ele.style[dir] = target.length + 'px'
-            clearInterval(timer)
+var menu = {
+    currentState: 'hide',
+    initialize: function () {
+        let self = this
+        self.on('hover', self.transition)
+    },
+    transition: function (evt) {
+        switch (this.currentState) {
+            case 'hide':
+                this.currentState = 'show'
+                break
+            case 'show':
+                this.currentState = 'hide'
+                break
+            default:
+                break
         }
-    }, 10)
+    }
 }
