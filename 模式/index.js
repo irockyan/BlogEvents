3ab6ad046892f3800b9e362d4faa8aa492510b77
Zyan <coderZyan@outlook.com>
2017-07-25 16:59:25 +0800

    构造器模式

---
+++ 模式/index.js
@@ -0,0 +1,15 @@
+function Person(name, age, sex) {
+    this.name = name 
+    this.age = age
+    this.sex = sex
+}
+
+Person.prototype.toString = function () {
+    console.log('这里是构造者模式' + this.age);
+}
+
+let zhaoLiu = new Person('Zyan', '20', 'male')
+zhaoLiu.toString()
+
+let wangWu = new Person('wang', '21', 'female')
+wangWu.toString()
