01f1b09cb628894f8c9bd52e0d929641e35873d3
Zyan <coderZyan@outlook.com>
2017-08-01 17:36:14 +0800

    提交

--- 单个表单项的检验/scripts/index.js
+++ 单个表单项的检验/scripts/index.js
@@ -6,18 +6,48 @@ window.onload = function () {
     const inputName = document.querySelector('.js_form_input')
     //获取验证按钮
     const verfyBtn = document.querySelector('.js_form_btn')
+    //要求
+    const ask = document.querySelector('.form_ask')
     //事件
     verfyBtn.addEventListener('click', function () {
-        verify(inputName)
+        // 总的字符数
+        let totalNum = 0
+        for (let le of inputName.value) {
+            if (le.charCodeAt(0) > 0xFF) {
+                totalNum += 2
+                console.log('文字')
+            } else {
+                console.log('字母')
+                totalNum += 1
+            }
+        }
+        console.log(totalNum)
+        if (totalNum < 4 || totalNum > 16) {
+            inputName
+                .classList
+                .remove('input_true')
+            ask
+                .classList
+                .remove('ask_true')
+            inputName
+                .classList
+                .add('input_err')
+            ask
+                .classList
+                .add('ask_err')
+            return
+        }
+        inputName
+            .classList
+            .remove('input_err')
+        ask
+            .classList
+            .remove('ask_err')
+        inputName
+            .classList
+            .add('input_true')
+        ask
+            .classList
+            .add('ask_true')
     })
-
-}
-
-/**
- * 
- * @param {*表单} ele
- */
-function verify(ele) {
-    //输入的值
-    let inputValue = ele.value
 }
